203d3123a8f54a6ed499ec88e0b0f422f55fe302
pablo peralta <pablofedericoperalta@gmail.com>
2025-11-17 18:50:14 -0300

    Modelos limpios de comentarios y navbar edited

--- modelos/reviews.js
+++ modelos/reviews.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const reviewSchema = new Schema({
-    body: String,
-    rating: Number,
-    autor: {
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario'
-    }
+  body: String,
+  rating: Number,
+  autor: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+  },
 });
 
-
-// Exportar para usar este schema/ modelo en otros docuentos 
 module.exports = mongoose.model('Review', reviewSchema);
--- modelos/usuario.js
+++ modelos/usuario.js
@@ -2,26 +2,21 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
-// We don't specify username or password in our user schema
-// but require that information on the following step 
 const usuarioSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    verificationToken: {
-        type: String
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
 });
 
-// it's going to add fields like name and password on our schema
 usuarioSchema.plugin(passportLocalMongoose);
-
-
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
