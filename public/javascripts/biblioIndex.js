0a63525dc98651c6eff35ec7c09edf0ca0602c68
pablo peralta <pablofedericoperalta@gmail.com>
2025-11-18 12:16:13 -0300

    javascriopt sin comentarios y views mejoradas

--- public/javascripts/biblioIndex.js
+++ public/javascripts/biblioIndex.js
@@ -1,49 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cardTitles = document.querySelectorAll('.biblioteca-item .card .row .col-md-8 .card-body h5.card-title');
-    const bibliotecaItems = document.querySelectorAll('.biblioteca-item');
+  const cardTitles = document.querySelectorAll(
+    '.biblioteca-item .card .row .col-md-8 .card-body h5.card-title'
+  );
+  const bibliotecaItems = document.querySelectorAll('.biblioteca-item');
 
-    function convertTitleToLink() {
-        if (window.innerWidth <= 460) {
-            cardTitles.forEach((title, index) => {
-                const link = bibliotecaItems[index].querySelector('.card-body .btn-primary');
-                if (link && !title.querySelector('a')) { 
-                    const href = link.getAttribute('href');
-                    const anchor = document.createElement('a');
-                    anchor.href = href;
-                    anchor.textContent = title.textContent;
-                    anchor.classList.add('card-link'); 
-                    title.textContent = '';
-                    title.appendChild(anchor); 
-                }
-            });
-        } else {
-            // Si la pantalla es mayor, revertimos los títulos a texto plano (opcional)
-            cardTitles.forEach(title => {
-                const anchor = title.querySelector('a');
-                if (anchor) {
-                    title.textContent = anchor.textContent;
-                    title.removeChild(anchor);
-                }
-            });
+  function convertTitleToLink() {
+    if (window.innerWidth <= 460) {
+      cardTitles.forEach((title, index) => {
+        const link = bibliotecaItems[index].querySelector(
+          '.card-body .btn-primary'
+        );
+        if (link && !title.querySelector('a')) {
+          const href = link.getAttribute('href');
+          const anchor = document.createElement('a');
+          anchor.href = href;
+          anchor.textContent = title.textContent;
+          anchor.classList.add('card-link');
+          title.textContent = '';
+          title.appendChild(anchor);
         }
+      });
+    } else {
+      cardTitles.forEach((title) => {
+        const anchor = title.querySelector('a');
+        if (anchor) {
+          title.textContent = anchor.textContent;
+          title.removeChild(anchor);
+        }
+      });
     }
+  }
 
- 
-    convertTitleToLink();
+  convertTitleToLink();
 
-
-    window.addEventListener('resize', convertTitleToLink);
+  window.addEventListener('resize', convertTitleToLink);
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
 
-
-        document.addEventListener('DOMContentLoaded', () => {
-            const form = document.querySelector('form');
-         
-            form.addEventListener('submit', (event) => {
-                event.preventDefault();
-                const queryString = new URLSearchParams(new FormData(form)).toString();
-                window.location.href = `/bibliotecas?${queryString}`; 
-            });
-        });
-
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const queryString = new URLSearchParams(new FormData(form)).toString();
+    window.location.href = `/bibliotecas?${queryString}`;
+  });
+});
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,19 +1,17 @@
-
-
 maptilersdk.config.apiKey = maptilerApiKey;
 
 const map = new maptilersdk.Map({
-    container: 'map',
-    style: maptilersdk.MapStyle.BRIGHT,
-    center: [bibliotecaData.coords.lon, bibliotecaData.coords.lat],
-    //biblioteca.geometry.coordinates, // starting position [lng, lat]
-    zoom: 8 // starting zoom
+  container: 'map',
+  style: maptilersdk.MapStyle.BRIGHT,
+  center: [bibliotecaData.coords.lon, bibliotecaData.coords.lat],
+  zoom: 8,
 });
 
 new maptilersdk.Marker()
-    .setLngLat([bibliotecaData.coords.lon, bibliotecaData.coords.lat]) //biblioteca.geometry.coordinates
-    .setPopup(
-        new maptilersdk.Popup({ offset: 25 })
-            .setHTML(`<h3>${bibliotecaData.nombre}</h3> <p>${bibliotecaData.localidad}</p>`)
+  .setLngLat([bibliotecaData.coords.lon, bibliotecaData.coords.lat])
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(
+      `<h3>${bibliotecaData.nombre}</h3> <p>${bibliotecaData.localidad}</p>`
     )
-    .addTo(map)
+  )
+  .addTo(map);
